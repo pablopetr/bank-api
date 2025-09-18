46e87801913a8f0ea03391d9b1274557e452ddce
Pablo Eliezer <pabloeliezer@hotmail.com>
2025-09-18 20:31:09 -0300

    Create action UpdateWallet

---
+++ app/Actions/Wallets/UpdateWallet.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Actions\Wallets;
+
+use App\Enums\WalletType;
+use App\Models\Wallet;
+use RuntimeException;
+
+class UpdateWallet
+{
+    public function execute(Wallet $wallet, string $name): Wallet
+    {
+        if($wallet->type === WalletType::Default) {
+            throw new RuntimeException("You cannot update the default wallet.");
+        }
+
+        $wallet->update([
+            'name' => $name,
+        ]);
+
+        return $wallet;
+    }
+}
---
+++ tests/Feature/Actions/Wallets/UpdateWalletTest.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Actions\Wallets\UpdateWallet;
+use App\Enums\WalletType;
+use App\Models\Wallet;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(RefreshDatabase::class);
+
+it('should be able to update wallet', function () {
+    $wallet = Wallet::factory()->create(['type' => WalletType::Wallet]);
+
+    (new UpdateWallet())->execute($wallet, $newName = 'Updated Wallet Name');
+
+    $this->assertDatabaseHas('wallets', [
+        'id' => $wallet->id,
+        'type' => WalletType::Wallet->value,
+        'name' => $newName,
+    ]);
+});
+
+it('should not be able to update default wallet', function () {
+    $wallet = Wallet::factory()->create(['type' => WalletType::Default]);
+
+    $newName = 'Updated Wallet Name';
+    expect(fn () => (new UpdateWallet())->execute($wallet, $newName))
+        ->toThrow(RuntimeException::class, "You cannot update the default wallet.");
+
+    $this->assertDatabaseHas('wallets', [
+        'id' => $wallet->id,
+        'type' => WalletType::Default->value,
+        'name' => $wallet->name,
+    ]);
+
+    $this->assertDatabaseMissing('wallets', [
+        'id' => $wallet->id,
+        'name' => $newName,
+    ]);
+});
