04bd9b0037b0674c63d75a8b386616067cd58206
Pablo Eliezer <pabloeliezer@hotmail.com>
2025-09-18 20:14:00 -0300

    Create action CreateCustomWallet

--- app/Actions/Wallets/CreateCustomWallet.php
+++ app/Actions/Wallets/CreateCustomWallet.php
@@ -5,11 +5,21 @@ namespace App\Actions\Wallets;
 use App\Enums\WalletType;
 use App\Models\Account;
 use App\Models\Wallet;
+use RuntimeException;
 
 class CreateCustomWallet
 {
     public function execute(Account $account, string $walletName): void
     {
+        $existentWallet = Wallet::query()
+            ->where('account_id', $account->id)
+            ->where('name', $walletName)
+            ->first();
+
+        if ($existentWallet) {
+            throw new RuntimeException("A wallet with name '{$walletName}' already exists in the account.");
+        }
+
         Wallet::query()->create([
             'name' => $walletName,
             'account_id' => $account->id,
--- tests/Feature/Actions/Wallets/CreateCustomWalletTest.php
+++ tests/Feature/Actions/Wallets/CreateCustomWalletTest.php
@@ -21,3 +21,18 @@ it('should be able to create custom wallet', function () {
         'account_id' => $account->id,
     ]);
 });
+
+it('should throw a runtime error when a wallet with same name exists in the account', function () {
+    $account = Account::factory()->create();
+
+    Wallet::factory()->create(['account_id' => $account->id, 'name' => $walletName = 'My Custom Wallet']);
+
+    $this->assertDatabaseCount(Wallet::class, 1);
+
+     $action = (new CreateCustomWallet());
+
+     expect(fn () => $action->execute($account, $walletName))
+         ->toThrow(RuntimeException::class, "A wallet with name '{$walletName}' already exists in the account.");
+
+    $this->assertDatabaseCount(Wallet::class, 1);
+});
