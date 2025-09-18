790b41d21d6019857a60c303cb8f25184e8be788
Pablo Eliezer <pabloeliezer@hotmail.com>
2025-09-18 20:23:36 -0300

    Create action DeleteWallet

---
+++ app/Actions/Wallets/DeleteWallet.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Actions\Wallets;
+
+use App\Enums\WalletType;
+use App\Models\Wallet;
+use RuntimeException;
+
+class DeleteWallet
+{
+    public function execute(int $walletId): void
+    {
+        $wallet = Wallet::query()->findOrFail($walletId);
+
+        if($wallet->balance > 0) {
+            throw new RuntimeException("Cannot delete a wallet with a positive balance.");
+        }
+
+        if($wallet->type === WalletType::Default) {
+            throw new RuntimeException("Cannot delete the default wallet.");
+        }
+
+        $wallet->delete();
+    }
+}
---
+++ tests/Feature/Actions/Wallets/DeleteWalletTest.php
@@ -0,0 +1,49 @@
+<?php
+
+use App\Actions\Wallets\DeleteWallet;
+use App\Enums\WalletType;
+use App\Models\Wallet;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(RefreshDatabase::class);
+
+it('should be able to delete a wallet', function () {
+    $wallet = Wallet::factory()->create(['balance' => 0, 'type' => WalletType::Wallet]);
+
+    $this->assertDatabaseCount(Wallet::class, 1);
+
+    (new DeleteWallet())->execute($wallet->id);
+
+    $this->assertDatabaseCount(Wallet::class, 1);
+    $this->assertSoftDeleted($wallet);
+});
+
+it('should not be able to delete a wallet with positive balance', function () {
+    $wallet = Wallet::factory()->create(['balance' => 0.01]);
+
+    $this->assertDatabaseCount(Wallet::class, 1);
+
+    expect(fn () => (new DeleteWallet())->execute($wallet->id))
+        ->toThrow(RuntimeException::class, 'Cannot delete a wallet with a positive balance.');
+
+    $this->assertDatabaseCount(Wallet::class, 1);
+
+    $wallet->refresh();
+
+    $this->assertNull($wallet->deleted_at);
+});
+
+it('should not be able to delete a default wallet', function () {
+    $wallet = Wallet::factory()->create(['balance' => 0, 'type' => WalletType::Default]);
+
+    $this->assertDatabaseCount(Wallet::class, 1);
+
+    expect(fn () => (new DeleteWallet())->execute($wallet->id))
+        ->toThrow(RuntimeException::class, 'Cannot delete the default wallet.');
+
+    $this->assertDatabaseCount(Wallet::class, 1);
+
+    $wallet->refresh();
+
+    $this->assertNull($wallet->deleted_at);
+});
